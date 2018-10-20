afef8441695120ccb1a3f3bcc086c6627fa7cac6
Justin Singh <justin.singh125125@gmail.com>
2018-10-19 21:22:30 -0700

    added random quote

--- assets/javascript/navbar.js
+++ assets/javascript/navbar.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
     $(window).on('scroll', function () {
         if ($(window).scrollTop()) {
             $('.navbar').css('background', 'black');
+            $('.navbar').css('height', '80px');
         }
         else {
             $('.navbar').css('background', 'transparent');
