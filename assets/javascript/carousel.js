c19403dc9b99e93d05712b8416c5660e177ddce7
Justin Singh <justin.singh125125@gmail.com>
2019-01-10 01:41:08 -0800

    added new project

--- assets/javascript/carousel.js
+++ assets/javascript/carousel.js
@@ -2,26 +2,26 @@ var counter = 0;
 var quoteArray = [];
 for (var i = 0; i <= 3; i++) {
     // http://quotes.rest/quote/random.json?api_key=<your_api_key>
-    var queryURL = "https://talaikis.com/api/quotes/random/";
+    var queryURL = "https://quota.glitch.me/random";
 
     $.ajax({
         url: queryURL,
         method: "GET",
     }).then(function (response) {
-
+        console.log(response);
         if (counter == 0) {
-            $('#random-quote-1').text(`"${response.quote}"`);
-            $('#author-1').text(response.author);
+            $('#random-quote-1').text(`"${response.quoteText}"`);
+            $('#author-1').text(response.quoteAuthor);
         }
 
         if (counter == 1) {
-            $('#random-quote-2').text(`"${response.quote}"`);
-            $('#author-2').text(response.author);
+            $('#random-quote-2').text(`"${response.quoteText}"`);
+            $('#author-2').text(response.quoteAuthor);
         }
 
         if (counter == 2) {
-            $('#random-quote-3').text(`"${response.quote}"`);
-            $('#author-3').text(response.author);
+            $('#random-quote-3').text(`"${response.quoteText}"`);
+            $('#author-3').text(response.quoteAuthor);
         }
 
         counter++;
